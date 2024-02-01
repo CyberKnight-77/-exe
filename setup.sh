6cfc39f9a7965657f7d7a365c9660b0889ddd444
CyberKnight <140019600+CyberKnight-77@users.noreply.github.com>
2024-02-01 21:09:59 +0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,11 @@
 apt install cloudflared
+
 apt install apache2
+
 rm $PREFIX/etc/apache2/httpd.conf
+
 mv httpd.conf $PREFIX/etc/apache2/
+
 apachectl -k start
+
 cloudflared tunnel --url localhost:8080
