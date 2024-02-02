68a3156d165d1d8815a914595568914056a64d52
CyberKnight <140019600+CyberKnight-77@users.noreply.github.com>
2024-02-02 10:09:52 +0600

    Update installx.sh

--- installx.sh
+++ installx.sh
@@ -1,4 +1,4 @@
 apt install apache2
-rm ...usr/etc/apache2/httpd.conf
-mv httpd.conf ...usr/etc/apache2
+rm /data/data/com.termux/files/usr/etc/apache2/httpd.conf
+mv httpd.conf /data/data/com.termux/files/usr/etc/apache2
 apachectl -k start
