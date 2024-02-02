98a4af5c645167230d14e013f02c64b7217a2950
CyberKnight <140019600+CyberKnight-77@users.noreply.github.com>
2024-02-02 09:57:35 +0600

    Update installx.sh

--- installx.sh
+++ installx.sh
@@ -1,4 +1,4 @@
 apt install apache2
-rm $PREFIX/usr/etc/apache2/httpd.conf
-mv httpd.conf $PREFIX/usr/etc/apache2
+rm .../usr/etc/apache2/httpd.conf
+mv httpd.conf .../usr/etc/apache2
 apachectl -k start
