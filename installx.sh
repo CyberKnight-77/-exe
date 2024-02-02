a643bc2148868c3fc539e3e49d8eef68267ba53d
CyberKnight <140019600+CyberKnight-77@users.noreply.github.com>
2024-02-02 10:48:58 +0600

    Update installx.sh

--- installx.sh
+++ installx.sh
@@ -2,5 +2,3 @@ apt install apache2
 rm /data/data/com.termux/files/usr/etc/apache2/httpd.conf
 cp httpd.conf /data/data/com.termux/files/usr/etc/apache2
 apachectl -k start
-apt install cloudflared
-cloudflared tunnel --url localhost:8080
